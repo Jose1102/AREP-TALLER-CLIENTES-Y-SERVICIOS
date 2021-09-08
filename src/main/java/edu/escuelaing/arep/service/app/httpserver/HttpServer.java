5fdfaa11e7849f0dcbc25971ff1dec2ca09c8e9a
Jose1102 <jose.castroortega1102@gmail.com>
2021-09-07 22:46:10 -0500

    procfile

--- src/main/java/edu/escuelaing/arep/service/app/httpserver/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/service/app/httpserver/HttpServer.java
@@ -23,7 +23,7 @@ public class HttpServer {
 	public static void main(String[] args) throws IOException {
 		ServerSocket serverSocket = null;
 		try {
-			serverSocket = new ServerSocket(35000);
+			serverSocket = new ServerSocket(getPort());
 		} catch (IOException e) {
 			System.err.println("Could not listen on port: 35000.");
 			System.exit(1);
@@ -81,5 +81,14 @@ public class HttpServer {
 		clientSocket.close();
 
 		serverSocket.close();
+		
+		
+	}
+	
+	public static int getPort() {
+		 if (System.getenv("PORT") != null) {
+			 return Integer.parseInt(System.getenv("PORT"));
+			 }
+			 return 35000;
 	}
 }
